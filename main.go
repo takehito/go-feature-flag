b40f4a8982882746a8b8ed9c0e61e4af1302d431
takehito <utisama.produce@gmail.com>
2020-04-04 10:48:29 +0900

    機能追加のためのフィーチャーフラグを追加

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+var feature bool
 
 func sum(a int, b int) int {
 	return a + b
 }
 
 func main() {
+	flag.BoolVar(&feature, "feature", false, "開発中の機能を実行するためのフラグ")
+	flag.Parse()
+
 	fmt.Println(sum(2, 7))
 }
